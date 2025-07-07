5863b0ebe6ef1f30d12445cf224fb8bf0dec4a72
Aram Zakaryan <aram.zakaryan@n.rich>
2025-07-07 23:34:19 +0400

    result of section 8

--- src/controllers/errors.controller.js
+++
@@ -1,6 +0,0 @@
-exports.getError404 = (req, res) => {
-    res.render('404', {
-        path: undefined,
-        pageTitle: "404 Page Not Found"
-    });
-}
--- src/routes/shop.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const productsController = require('../controllers/products.controller')
-const shopRouter = express.Router();
-
-
-shopRouter.get('/', productsController.getProduct)
-
-module.exports = shopRouter;
